7ca0b3fd9f6ff7f6bfeb3dcac94f36d4fa1db27d
Yuefeng Wang <93065511+euphw@users.noreply.github.com>
2022-09-07 22:44:23 -0400

    Keeper App part 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (<footer>
+        <p>Copyright ⓒ {year}</p>
+    </footer>);
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+//import { StrictMode } from "react";
+
+function Heading() {
+    return (
+    <header>
+        <h1>Keeper</h1>
+    </header>);
+}
+
+export default Heading;
